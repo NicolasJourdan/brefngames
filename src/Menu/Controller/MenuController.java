046499ad81583b5ca99d381ce420b2c27600ba65
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2018-11-12 18:48:40 +0100

    Added menu

--- src/Menu/Controller/MenuController.java
+++ src/Menu/Controller/MenuController.java
@@ -1,20 +1,13 @@
 package Menu.Controller;
 
 import Scene.Controller.AbstractSceneController;
-import Scene.Model.ActionEnum;
 import Scene.Model.Scene;
 import Structure.AbstractModel;
 import Structure.AbstractView;
-
-import java.util.Observable;
 
 public class MenuController extends AbstractSceneController {
 
     public MenuController(AbstractModel model, AbstractView view, Scene scene) {
         super(model, view, scene);
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
     }
 }
--- src/Menu/View/MenuView.java
+++ src/Menu/View/MenuView.java
@@ -1,15 +1,74 @@
 package Menu.View;
 
+import Scene.Model.ActionEnum;
 import Structure.AbstractView;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MenuView extends AbstractView {
 
+    private final JButton trainingButton;
+    private final JButton contestButton;
+    private final JButton parametersButton;
+    private final JButton statisticsButton;
+    private final JButton quitButton;
+
     public MenuView() {
+        super();
         this.setLayout(new GridLayout());
-        this.add(new Button("toto"));
-        this.add(new Button("toto"));
-        this.add(new Button("toto"));
+
+        this.trainingButton = new JButton("Training");
+        this.contestButton = new JButton("Contest");
+        this.parametersButton = new JButton("Parameters");
+        this.statisticsButton = new JButton("Statistics");
+        this.quitButton = new JButton("Quit");
+
+        this.initButtonsActionListeners();
+
+        this.add(this.trainingButton);
+        this.add(this.contestButton);
+        this.add(this.parametersButton);
+        this.add(this.statisticsButton);
+        this.add(this.quitButton);
+    }
+
+    private void initButtonsActionListeners() {
+        this.trainingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MenuView.this.observable.notifyObservers(ActionEnum.TRAINING);
+            }
+        });
+
+        this.contestButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MenuView.this.observable.notifyObservers(ActionEnum.CONTEST);
+            }
+        });
+
+        this.parametersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MenuView.this.observable.notifyObservers(ActionEnum.PARAMETERS);
+            }
+        });
+
+        this.statisticsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MenuView.this.observable.notifyObservers(ActionEnum.STATISTICS);
+            }
+        });
+
+        this.quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MenuView.this.observable.notifyObservers(ActionEnum.QUIT);
+            }
+        });
     }
 }
