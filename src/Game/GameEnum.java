a8d55decc9b3b8cb8aeafbf5f6d8972dd997ec1b
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2018-11-13 20:19:50 +0100

    47 - Added GameEnum and added player action

--- src/Game/GameEnum.java
+++ src/Game/GameEnum.java
@@ -1,7 +1,34 @@
 package Game;
 
 /**
- * List of all games
+ * List of all game enum
  */
 public enum GameEnum {
+
+    TIC_TAC_TOE("TIC_TAC_TOE"),
+    CONNECT_FOUR("CONNECT_FOUR"),
+    RUNNER("RUNNER"),
+    COOKIE_CLICKER("COOKIE_CLICKER"),
+    ;
+
+    private String name = "";
+
+    /**
+     * Create a game enum.
+     */
+    GameEnum(String s)
+    {
+        this.name = s;
+    }
+
+    /**
+     * The method to display the name
+     *
+     * @return name is the name of the game enum
+     */
+    @Override public String toString()
+    {
+        return this.name;
+    }
+
 }
