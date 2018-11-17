782f9173b32e73d84a1c4f878ffca3f6e7959390
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2018-11-17 14:49:33 +0100

    38 - Added ParametersMenuScene

---
+++ src/Parameter/Controller/ParametersMenuController.java
@@ -0,0 +1,12 @@
+package Parameter.Controller;
+
+import Scene.Controller.AbstractSceneController;
+import Structure.AbstractModel;
+import Structure.AbstractView;
+
+public class ParametersMenuController extends AbstractSceneController {
+
+    public ParametersMenuController(AbstractModel model, AbstractView view) {
+        super(model, view);
+    }
+}
