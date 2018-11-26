0d7094dcef901815e050196930e1d838a8499d7a
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2018-11-26 01:15:06 +0100

    58 - Added CookieClicker Repository

---
+++ src/Repository/Game/CookieClickerRepository.java
@@ -0,0 +1,56 @@
+package Repository.Game;
+
+import Game.Games.CookieClicker.CookieClickerStatsEnum;
+import Repository.ModifyFiles;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CookieClickerRepository extends AbstractGameRepository {
+    private static final String DEFAULT_NODE = "cookieClicker";
+    private static final String NAME = "name";
+    private static final String VALUE = "value";
+
+    public static Map<CookieClickerStatsEnum, String> getAll() {
+        Map<CookieClickerStatsEnum, String> stats = new HashMap<>();
+
+        JSONArray statsJSONArray = (JSONArray) getDataFile().get(DEFAULT_NODE);
+        Iterator<JSONObject> statsIterator = statsJSONArray.iterator();
+
+        while (statsIterator.hasNext()) {
+            JSONObject current = statsIterator.next();
+            stats.put(CookieClickerStatsEnum.valueOf((String) current.get(NAME)), (String) current.get(VALUE));
+        }
+
+        return stats;
+    }
+
+    public static String getById(CookieClickerStatsEnum statName) {
+        return getAll().get(statName);
+    }
+
+    /**
+     * Save ALL the cookie clicker stats
+     * @param stats Map with ALL keys
+     */
+    public static void saveAll(Map<CookieClickerStatsEnum, String> stats) {
+        JSONArray statsJSONArray = new JSONArray();
+        for (Map.Entry<CookieClickerStatsEnum, String> entry : stats.entrySet()) {
+            JSONObject current = new JSONObject();
+            current.put(NAME, entry.getKey().toString());
+            current.put(VALUE, entry.getValue());
+            statsJSONArray.add(current);
+        }
+
+        // Create a copy
+        ModifyFiles.saveJSONFile(DATA_JSON_FILE);
+
+        JSONObject dataJSON = getDataFile();
+        dataJSON.put(DEFAULT_NODE, statsJSONArray);
+
+        ModifyFiles.write(DATA_JSON_FILE, dataJSON);
+    }
+}
