5daf2c6a82a39e0212c41675e68ee250ff7a5590
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2018-11-26 23:32:24 +0100

    67 - Create strucute of the service

---
+++ src/Contest/Model/ContestDataPersistor.java
@@ -0,0 +1,27 @@
+package Contest.Model;
+
+import Game.Games.ConnectFour.ConnectFourStatsEnum;
+import Game.Games.CookieClicker.CookieClickerStatsEnum;
+import Game.Games.Runner.RunnerStatsEnum;
+import Game.Games.TicTacToe.TicTacToeStatsEnum;
+
+import java.util.Map;
+
+public class ContestDataPersistor {
+
+    public static void updateTicTacToe(Map<TicTacToeStatsEnum, String> gameMap) {
+        // TODO update repo
+    }
+
+    public static void updateRunner(Map<RunnerStatsEnum, String> gameMap) {
+        // TODO update repo
+    }
+
+    public static void updateCookieCliker(Map<CookieClickerStatsEnum, String> gameMap) {
+        // TODO update repo
+    }
+
+    public static void updateConnectFour(Map<ConnectFourStatsEnum, String> gameMap) {
+        // TODO update repo
+    }
+}
