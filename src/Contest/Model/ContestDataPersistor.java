1d6c20414610720f1712ed8317fef70a2140713b
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2018-11-27 23:33:42 +0100

    66 - Added Runner data persistor

--- src/Contest/Model/ContestDataPersistor.java
+++ src/Contest/Model/ContestDataPersistor.java
@@ -4,6 +4,7 @@ import Game.Games.ConnectFour.ConnectFourStatsEnum;
 import Game.Games.CookieClicker.CookieClickerStatsEnum;
 import Game.Games.Runner.RunnerStatsEnum;
 import Game.Games.TicTacToe.TicTacToeStatsEnum;
+import Repository.Game.RunnerRepository;
 import Repository.Game.TicTacToeRepository;
 
 import java.util.Map;
@@ -42,8 +43,41 @@ public class ContestDataPersistor {
     }
 
     public static void updateRunner(Map<RunnerStatsEnum, String> gameMap) {
-        // TODO update repo
+        Map<RunnerStatsEnum, String> dataEntries = RunnerRepository.getAll();
+        updateIntValueRunner(dataEntries, gameMap, RunnerStatsEnum.RUNNER_NB_GAMES);
+        updateIntValueRunner(dataEntries, gameMap, RunnerStatsEnum.RUNNER_NB_CLICS);
+        updateIntValueRunner(dataEntries, gameMap, RunnerStatsEnum.RUNNER_TOTAL_TIME);
+        // Average time of each race
+        dataEntries.put(
+                RunnerStatsEnum.RUNNER_AVERAGE_TIME,
+                String.valueOf(
+                        Integer.parseInt(dataEntries.get(RunnerStatsEnum.RUNNER_TOTAL_TIME))
+                                / Integer.parseInt(dataEntries.get(RunnerStatsEnum.RUNNER_NB_GAMES))
+                )
+        );
+
+        // Average clicks per second
+        dataEntries.put(
+                RunnerStatsEnum.RUNNER_AVERAGE_SPEED,
+                String.valueOf(
+                        Integer.parseInt(dataEntries.get(RunnerStatsEnum.RUNNER_NB_CLICS))
+                                / Integer.parseInt(dataEntries.get(RunnerStatsEnum.RUNNER_TOTAL_TIME))
+                )
+        );
+        //TODO Add best player in playerStatsRepository
+        RunnerRepository.saveAll(dataEntries);
     }
+
+    private static void updateIntValueRunner(Map<RunnerStatsEnum, String> dataEntries, Map<RunnerStatsEnum, String> gamesEntries, RunnerStatsEnum stats) {
+        dataEntries.put(
+                stats,
+                String.valueOf(
+                        Integer.parseInt(gamesEntries.get(stats))
+                                + Integer.parseInt(dataEntries.get(stats))
+                )
+        );
+    }
+
 
     public static void updateCookieCliker(Map<CookieClickerStatsEnum, String> gameMap) {
         // TODO update repo
