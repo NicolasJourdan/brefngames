db98eddcd6fe75266a86ea62b6fee65677e24313
eliasb <elias.boulharts@cpe.fr>
2018-12-04 20:16:28 +0100

    141 - Bug fixed

--- src/Game/Games/TicTacToe/TicTacToeModel/TicTacToeModel.java
+++ src/Game/Games/TicTacToe/TicTacToeModel/TicTacToeModel.java
@@ -72,12 +72,17 @@ public class TicTacToeModel extends AbstractGameModel {
 
     public String setPawnModel(Coord coord) {
         Pawn pawn;
+        Pawn status;
         if (this.currentPlayer.equals(this.listPlayers[0])) {
             pawn = new Circle(this.currentPlayer, coord);
         } else {
             pawn = new Cross(this.currentPlayer, coord);
         }
-        return board.setPawn(pawn).toString();
+        status = board.setPawn(pawn);
+        if(status != null){
+            return status.toString();
+        }
+        return "";
     }
 
     public void changePlayer() {
