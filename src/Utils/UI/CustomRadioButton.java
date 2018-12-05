33a1051b87f7c2118d9a7865948f557332c8987f
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2018-12-05 07:32:10 +0100

    162 - Added font on JRadioButton

--- src/Utils/UI/CustomRadioButton.java
+++ src/Utils/UI/CustomRadioButton.java
@@ -6,5 +6,6 @@ public class CustomRadioButton extends JRadioButton {
     public CustomRadioButton(String text) {
         super(text);
         TraitJButton.traitSetSelected(this);
+        this.setFont(FileGetter.getFont().deriveFont(Utils.DEFAULT_SIZE_BUTTON_TEXT));
     }
 }
