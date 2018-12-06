4cf713361ec50d269160a5c11769566c82e6cd79
florian <florian.david.merle@gmail.com>
2018-12-06 21:36:37 +0100

    146 - fix custom checkbox view

--- src/Utils/UI/CustomRadioButton.java
+++ src/Utils/UI/CustomRadioButton.java
@@ -5,7 +5,10 @@ import javax.swing.*;
 public class CustomRadioButton extends JRadioButton {
     public CustomRadioButton(String text) {
         super(text);
-        TraitJButton.traitSetSelected(this);
+
+        this.setIcon(FileGetter.getImageIcon("_circle.png"));
+        this.setSelectedIcon(FileGetter.getImageIcon("_boxTick.png"));
+
         this.setFont(FileGetter.getFont().deriveFont(Utils.DEFAULT_SIZE_BUTTON_TEXT));
     }
 }
