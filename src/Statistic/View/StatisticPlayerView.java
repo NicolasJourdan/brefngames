1481b34fe958ae8164b81a3b5495bb22976c2dc0
Plaza Bastien <plaza.bastien@neuf.fr>
2018-12-10 17:41:13 +0100

    121 - view player

--- src/Statistic/View/StatisticPlayerView.java
+++ src/Statistic/View/StatisticPlayerView.java
@@ -3,6 +3,9 @@ package Statistic.View;
 import Scene.Model.ActionEnum;
 import Structure.AbstractView;
 import Utils.UI.CustomeComboBox.CustomComboBox;
+import Utils.UI.CustomButton;
+import Utils.UI.CustomLabel;
+import Utils.UI.Utils;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -13,40 +16,48 @@ import java.awt.event.ActionListener;
 import static Scene.Model.ActionEnum.STATISTIC_PLAYER_CHANGE;
 
 public class StatisticPlayerView extends AbstractView {
-    private JButton backButton;
+    private CustomButton backButton;
 
     private JTable tableau;
     private DefaultTableModel model;
     private GridBagConstraints c;
     private Object[][] dataTable;
-    private JComboBox jcb;
+    private CustomComboBox jcb;
+    private CustomLabel playerSettings;
+
 
     private String currentPlayer;
 
 
     public StatisticPlayerView() {
         super();
-
         this.currentPlayer = "";
 
         this.dataTable = new Object[][]{};
+
         this.setLayout(new GridBagLayout());
         this.c = new GridBagConstraints();
+        this.c.insets = new Insets(5, 0, 5, 0);
 
         this.c.fill = GridBagConstraints.HORIZONTAL;
 
-        this.jcb  = new CustomComboBox();
+        this.playerSettings = new CustomLabel("Player Statistic");
+        this.playerSettings.setFont(playerSettings.getFont().deriveFont(Utils.DEFAULT_SIZE_TITLE_LABEL));
+
         this.c.gridx = 0;
         this.c.gridy = 0;
-        this.c.gridwidth = 2;   // largeur 2 colonne
+        this.add(this.playerSettings, c);
+
+        this.jcb  = new CustomComboBox();
+        this.c.gridx = 0;
+        this.c.gridy = 1;
         this.add(jcb, c);
 
         this.updateTableView();
 
-        this.backButton = new JButton("Back");
-        this.c.gridx = 1;
+        this.backButton = new CustomButton("Back");
+        this.c.gridx = 0;
         this.c.gridy = 5;
-        this.c.gridwidth = 2;   // largeur 2 colonne
         this.add(this.backButton, c);
 
         this.initButtonsActionListeners();
@@ -76,14 +87,14 @@ public class StatisticPlayerView extends AbstractView {
     }
 
     public void updateTableView(){
+        this.c.fill = GridBagConstraints.HORIZONTAL;
         this.model = new DefaultTableModel(this.dataTable, new String[]{"statistic", "numbers"});
         this.tableau = new JTable(this.model);
         this.c.gridx = 0;
         this.c.gridy = 2;
-        this.c.gridwidth = 4;   // largeur 4 colonne
         this.add(this.tableau, c);
         this.tableau.setTableHeader(null);
-    }
+}
 
     public void updateJCBView(String[] players){
         this.jcb.removeAllItems();
