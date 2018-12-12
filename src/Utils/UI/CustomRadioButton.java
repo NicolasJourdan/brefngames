181bd2256b2419ce8dab7b701780dc7c83450b9c
florian <florian.david.merle@gmail.com>
2018-12-12 11:58:09 +0100

    200 - fix no sound radio button

--- src/Utils/UI/CustomRadioButton.java
+++ src/Utils/UI/CustomRadioButton.java
@@ -1,6 +1,8 @@
 package Utils.UI;
 
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class CustomRadioButton extends JRadioButton {
     public CustomRadioButton(String text) {
@@ -10,5 +12,18 @@ public class CustomRadioButton extends JRadioButton {
         this.setSelectedIcon(FileGetter.getImageIcon("_boxTick.png"));
 
         this.setFont(FileGetter.getFont().deriveFont(Utils.DEFAULT_SIZE_BUTTON_TEXT));
+
+        this.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (CustomRadioButton.this.model.isPressed()) {
+                    CustomRadioButton.this.playSound();
+                }
+            }
+        });
+    }
+
+    private void playSound() {
+        SoundPlayer.playSound(Utils.DEFAULT_CLICK_SOUND);
     }
 }
