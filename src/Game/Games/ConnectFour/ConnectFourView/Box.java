d2398c2277c5d0f1a2cc3a43a25a4289ee3f2d06
eliasb <elias.boulharts@cpe.fr>
2018-12-12 12:15:34 +0100

    184 - Set constants

--- src/Game/Games/ConnectFour/ConnectFourView/Box.java
+++ src/Game/Games/ConnectFour/ConnectFourView/Box.java
@@ -9,6 +9,30 @@ import java.awt.*;
 
 
 public class Box extends JButton {
+    private static int BOARD_SHADOW_X = 0;
+    private static int BOARD_SHADOW_Y = 0;
+    private static int BOARD_SHADOW_DX = 5;
+    private static int BOARD_SHADOW_DY = 5;
+    private static int HOLE_X = 3;
+    private static int HOLE_Y = 3;
+    private static int HOLE_DX = 5;
+    private static int HOLE_DY = 5;
+    private static int PAWN_SHADOW_X = 6;
+    private static int PAWN_SHADOW_Y = 6;
+    private static int PAWN_SHADOW_DX = 11;
+    private static int PAWN_SHADOW_DY = 11;
+    private static int PAWN_X = 4;
+    private static int PAWN_Y = 4;
+    private static int PAWN_DX = 11;
+    private static int PAWN_DY = 11;
+    private static int PAWN_STRIPE_X = 11;
+    private static int PAWN_STRIPE_Y = 11;
+    private static int PAWN_STRIPE_DX = 25;
+    private static int PAWN_STRIPE_DY = 25;
+    private static int PAWN_MIDDLE_X = 13;
+    private static int PAWN_MIDDLE_Y = 13;
+    private static int PAWN_MIDDLE_DX = 29;
+    private static int PAWN_MIDDLE_DY = 29;
     private final Coord coord;
     private Color backgroundColor;
     private Color emptyColor;
@@ -25,10 +49,9 @@ public class Box extends JButton {
         this.emptyColor = (Color) ThemeParameterRepository.getColor(ThemeEnum.SECOND_COLOR).getValue();
         this.color = (Color) ThemeParameterRepository.getColor(ThemeEnum.SECOND_COLOR).getValue();
         this.borderColor = (Color) ThemeParameterRepository.getColor(ThemeEnum.SECOND_COLOR).getValue();
+        this.opositeColor = Color.BLACK;
         if(ThemeParameterRepository.getColor(ThemeEnum.SECOND_COLOR).getValue().equals(Color.BLACK)){
             this.opositeColor = Color.WHITE;
-        } else {
-            this.opositeColor = Color.BLACK;
         }
     }
 
@@ -36,18 +59,24 @@ public class Box extends JButton {
     public void paint(Graphics g){
         g.setColor(this.backgroundColor);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        //board shadow
         g.setColor(this.opositeColor);
-        g.fillOval(0, 0, this.getWidth() - 5, this.getHeight() - 5);
+        g.fillOval(BOARD_SHADOW_X, BOARD_SHADOW_Y, this.getWidth() - BOARD_SHADOW_DX, this.getHeight() - BOARD_SHADOW_DY);
+        //hole
         g.setColor(this.emptyColor);
-        g.fillOval(3, 3, this.getWidth() - 5, this.getHeight() - 5);
+        g.fillOval(HOLE_X, HOLE_Y, this.getWidth() - HOLE_DX, this.getHeight() - HOLE_DY);
+        //pawn shadow
         g.setColor(this.borderColor);
-        g.fillOval(6, 6, this.getWidth() - 11, this.getHeight() - 11);
+        g.fillOval(PAWN_SHADOW_X, PAWN_SHADOW_Y, this.getWidth() - PAWN_SHADOW_DX, this.getHeight() - PAWN_SHADOW_DY);
+        //pawn
         g.setColor(this.color);
-        g.fillOval(4, 4, this.getWidth() - 11, this.getHeight() - 11);
+        g.fillOval(PAWN_X, PAWN_Y, this.getWidth() - PAWN_DX, this.getHeight() - PAWN_DY);
+        //pawn stripe
         g.setColor(this.borderColor);
-        g.fillOval(11, 11, this.getWidth() - 25, this.getHeight() - 25);
+        g.fillOval(PAWN_STRIPE_X, PAWN_STRIPE_Y, this.getWidth() - PAWN_STRIPE_DX, this.getHeight() - PAWN_STRIPE_DY);
+        //center of pawn
         g.setColor(this.color);
-        g.fillOval(13, 13, this.getWidth() - 29, this.getHeight() - 29);
+        g.fillOval(PAWN_MIDDLE_X, PAWN_MIDDLE_Y, this.getWidth() - PAWN_MIDDLE_DX, this.getHeight() - PAWN_MIDDLE_DY);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     }
