0a866b79021382e19b80729f73ae8a2a7a09d680
florian <florian.david.merle@gmail.com>
2018-12-15 22:46:10 +0100

    204 - fix combobox sound

--- src/Utils/UI/CustomComboBox/CustomComboBox.java
+++ src/Utils/UI/CustomComboBox/CustomComboBox.java
@@ -3,10 +3,15 @@ package Utils.UI.CustomComboBox;
 import Parameter.Model.ThemeEnum;
 import Repository.Parameter.ThemeParameterRepository;
 import Utils.UI.FileGetter;
+import Utils.UI.SoundPlayer;
 import Utils.UI.Utils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class CustomComboBox extends JComboBox {
 
@@ -23,5 +28,24 @@ public class CustomComboBox extends JComboBox {
         this.setRenderer(new CustomComboBoxRenderer());
 
         this.setMaximumRowCount(MAXIMUM_ROW_COUNT);
+
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CustomComboBox.this.playSound();
+            }
+        });
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                CustomComboBox.this.playSound();
+            }
+        });
+    }
+
+    private void playSound() {
+        SoundPlayer.playSound(Utils.DEFAULT_CLICK_SOUND);
     }
 }
