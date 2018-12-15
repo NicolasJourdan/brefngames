ef177ff5a2ef42ff61f6e2d7fe346ff24c5a5b45
florian <florian.david.merle@gmail.com>
2018-12-15 22:31:21 +0100

    215 - player name length validation

--- src/ContestSettings/Model/ContestSettingsModel.java
+++ src/ContestSettings/Model/ContestSettingsModel.java
@@ -10,6 +10,9 @@ import java.util.Map;
 
 public class ContestSettingsModel extends AbstractModel {
 
+    private static final int PLAYER_NAME_MAX_LENGTH = 10;
+    private static final int PLAYER_NAME_MIN_LENGTH = 3;
+
     private Map<ParameterEnum, Configurable> defaultConfiguration;
     private String invalidDataObjectText;
 
@@ -35,6 +38,12 @@ public class ContestSettingsModel extends AbstractModel {
             this.invalidDataObjectText += "At least one game type has to be selected<br>";
         }
 
+        if (!this.validatePlayerNameLength(settingsDataObject.getFirstPlayerName()) ||
+            !this.validatePlayerNameLength(settingsDataObject.getSecondPlayerName())) {
+            this.invalidDataObjectText += "Player name must have between " + ContestSettingsModel.PLAYER_NAME_MIN_LENGTH +
+                    " and " + ContestSettingsModel.PLAYER_NAME_MAX_LENGTH + "characters<br>";
+        }
+
         if (settingsDataObject.getFirstPlayerName().equals(settingsDataObject.getSecondPlayerName())) {
             this.invalidDataObjectText += "Both player can't have the same name<br>";
         }
@@ -49,6 +58,11 @@ public class ContestSettingsModel extends AbstractModel {
         }
 
         return true;
+    }
+
+    private boolean validatePlayerNameLength(String name) {
+        return name.length() <= ContestSettingsModel.PLAYER_NAME_MAX_LENGTH &&
+                name.length() >= ContestSettingsModel.PLAYER_NAME_MIN_LENGTH;
     }
 
     /**
