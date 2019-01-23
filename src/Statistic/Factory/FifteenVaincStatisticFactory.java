e5e5019a3d2a55c4a874ba6a3215c7cd42ce00e0
Plaza Bastien <plaza.bastien@neuf.fr>
2019-01-23 13:53:43 +0100

    309 - Stat Menu add new fonctionalities and resolve problem

--- src/Statistic/Factory/FifteenVaincStatisticFactory.java
+++ src/Statistic/Factory/FifteenVaincStatisticFactory.java
@@ -6,15 +6,15 @@ public class FifteenVaincStatisticFactory {
     public static String getStringStat(FifteenVaincStatsEnum stat) {
         switch (stat) {
             case FIFTEEN_VAINC_NB_GAMES:
-                return "Tic tac toe number of games";
+                return "Fifteen Vainc number of games";
             case FIFTEEN_VAINC_NB_DRAW:
-                return "Tic tac toe number of draws";
+                return "Fifteen Vainc number of draws";
             case FIFTEEN_VAINC_NB_PERFECT:
-                return "Tic tac toe number of perfects";
+                return "Fifteen Vainc number of perfects";
             case FIFTEEN_VAINC_TOTAL_TIME:
-                return "Tic tac toe total time";
+                return "Fifteen Vainc total time";
             case FIFTEEN_VAINC_BEST_PLAYER:
-                return "Tic tac toe best player";
+                return "Fifteen Vainc best player";
             default:
                 throw new RuntimeException("The statistic : " + stat.toString() + " is unknown");
         }
