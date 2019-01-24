620ef72fb56680a22147e5758a9a8f1efddcb667
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2019-01-24 17:23:52 +0100

    360 - Update back to next on back button

--- src/Map/Controller/ClientMapController.java
+++ src/Map/Controller/ClientMapController.java
@@ -1,5 +1,6 @@
 package Map.Controller;
 
+import Contest.Interface.SocketObserverController;
 import Game.Model.AbstractGameModel;
 import Game.View.AbstractGameView;
 import Map.Model.History;
@@ -10,14 +11,22 @@ import Online.Socket.SocketCommunicatorService;
 import Scene.Model.ActionEnum;
 
 import java.util.Observable;
+import java.util.Observer;
 
-public class ClientMapController extends MapController {
+public class ClientMapController extends MapController implements SocketObserverController {
+
+    private static final String BACK_BUTTON_TEXT = "next";
 
     private final SocketCommunicatorService socketCommunicatorService;
+    private final SocketReceptionService socketReceptionService;
+
 
     public ClientMapController(AbstractGameModel model, AbstractGameView view, History history, SocketCommunicatorService socketCommunicatorService) {
         super(model, view, history);
+
         this.socketCommunicatorService = socketCommunicatorService;
+        this.socketReceptionService = new SocketReceptionService();
+        this.socketCommunicatorService.addReceptionObserver(this.socketReceptionService);
     }
 
     @Override
@@ -27,6 +36,24 @@ public class ClientMapController extends MapController {
                 ((MapView) this.view).setWaitingButton();
                 this.socketCommunicatorService.emit(new MessageDataObject(MessageType.MAP_NEXT));
                 break;
+        }
+    }
+
+    @Override
+    public void stopObserver() {
+        this.socketCommunicatorService.deleteReceptionObserver(this.socketReceptionService);
+    }
+
+    private class SocketReceptionService implements Observer {
+        @Override
+        public void update(Observable o, Object arg) {
+            MessageDataObject messageDataObject = (MessageDataObject) arg;
+
+            switch (messageDataObject.getType()) {
+                case MAP_FINISH:
+                    ((MapView) ClientMapController.this.view).setBackButton(ClientMapController.BACK_BUTTON_TEXT);
+                    break;
+            }
         }
     }
 }
